ce2b8d54a8d7c1ac1adc4d0343b8b63c7afebb9a
rachelnguyen <rachelnguyn@gmail.com>
2022-02-14 15:36:45 -0800

    Update resume.js

--- pages/resume.js
+++ pages/resume.js
@@ -9,7 +9,7 @@ export default function FirstPost() {
         <title>resume</title>
       </Head>
       <div className="resume">
-      <iframe className="resume" src="https://docs.google.com/document/d/e/2PACX-1vQ4pdONHdM7lH706l2LWszyNbcGxNoNlJnaS1ovn7lsXCzbbvrfh5cBqi8cXGfad5aeeoRxk0nqCK1O/pub?embedded=true"  frameborder="0"></iframe>
+      <iframe className="resume" src="https://docs.google.com/document/d/e/2PACX-1vQ4pdONHdM7lH706l2LWszyNbcGxNoNlJnaS1ovn7lsXCzbbvrfh5cBqi8cXGfad5aeeoRxk0nqCK1O/pub?embedded=true"  frameBorder="0"></iframe>
  
       <style jsx>{`
         .resume{
