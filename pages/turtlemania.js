e4f6ea2f042e460a52a757faad0131c105d2144a
rachelnguyen <rachelnguyn@gmail.com>
2022-02-22 20:36:07 -0800

    Create turtlemania.js

---
+++ pages/turtlemania.js
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+import Head from 'next/head'
+
+
+export default function TurtleMania() {
+  return (
+    <>
+      <Head>
+        <title>turtle mania</title>
+      </Head>
+      <div className="turtle mania">
+ 
+      <style jsx>{`
+      `}</style>
+ </div>
+ </>
+  )
+}
